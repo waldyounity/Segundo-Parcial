1a80ffcaac8c1c242f439bead54ccb7765468099
waldyounity <91232516+waldyounity@users.noreply.github.com>
2026-03-10 20:42:38 -0400

    proteccion de rutas desde afuera

--- app/extensions.py
+++ app/extensions.py
@@ -1,11 +1,23 @@
+from flask import redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_admin import Admin
+from flask_login import LoginManager, current_user
+from flask_admin import Admin, AdminIndexView
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 
-admin = Admin(name="Soporte Técnico - IT")
+# 1. Creamos el guardia para la ruta principal del panel
+class PanelSeguroView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        flash("Debes iniciar sesión para acceder al panel administrativo.", "warning")
+        return redirect(url_for("auth.login"))
+
+# 2. Le inyectamos el guardia al Admin en el parámetro index_view
+admin = Admin(name="Soporte Técnico - IT", index_view=PanelSeguroView())
+
 login_manager.login_view = "auth.login"
